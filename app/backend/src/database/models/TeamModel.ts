239634cb88b34b824af03b4c95cdeea904b9cccf
Murilo-MRS <murilosantana1993@gmail.com>
2023-02-27 17:31:45 -0300

    feat: req 1 - model e migration

---
+++ app/backend/src/database/models/TeamModel.ts
@@ -0,0 +1,28 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class TeamModel extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+TeamModel.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  teamName: {
+    allowNull: false,
+    type: STRING,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default TeamModel;
