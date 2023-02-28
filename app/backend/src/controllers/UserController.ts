801e807508f61b6ac329d57251e70675e006f722
Murilo-MRS <murilosantana1993@gmail.com>
2023-02-28 19:49:41 -0300

    feat: req 8 - Controller - /login login

---
+++ app/backend/src/controllers/UserController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import IUserService from '../interfaces/IUserService';
+
+class UserController {
+  private _service: IUserService;
+
+  constructor(service: IUserService) {
+    this._service = service;
+  }
+
+  public async login(req: Request, res: Response): Promise<Response | void> {
+    const { status, message } = await this._service.login(req.body);
+    return res.status(status).json({ message });
+  }
+}
+
+export default UserController;
