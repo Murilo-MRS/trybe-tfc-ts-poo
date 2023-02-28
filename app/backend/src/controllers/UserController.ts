c327a68895c78b926651d50d29ba64a61fa9a444
Murilo-MRS <murilosantana1993@gmail.com>
2023-02-28 20:47:04 -0300

    fix: req 8 - Controller - /login login

--- app/backend/src/controllers/UserController.ts
+++ app/backend/src/controllers/UserController.ts
@@ -8,7 +8,7 @@ class UserController {
     this._service = service;
   }
 
-  public async login(req: Request, res: Response): Promise<Response | void> {
+  public async login(req: Request, res: Response) {
     const { status, message } = await this._service.login(req.body);
     return res.status(status).json({ message });
   }
