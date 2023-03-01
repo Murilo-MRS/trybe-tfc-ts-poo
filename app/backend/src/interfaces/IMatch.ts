5fa092ccab9c99ea27e6a108fc3b9b61c6b7770e
Murilo-MRS <murilosantana1993@gmail.com>
2023-03-01 15:47:28 -0300

    feat: req 15 - inetrfaces - matches

---
+++ app/backend/src/interfaces/IMatch.ts
@@ -0,0 +1,14 @@
+export default interface IMatch {
+  id?: number,
+  homeTeamId: number,
+  homeTeamGoals: number,
+  awayTeamId: number,
+  awayTeamGoals: number,
+  inProgress: boolean,
+  homeTeam: {
+    teamName: string,
+  },
+  awayTeam: {
+    teamName: string,
+  },
+}
