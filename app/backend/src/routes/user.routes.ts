8eaf6fce185ed35f841191ec08c766ada0888373
Murilo-MRS <murilosantana1993@gmail.com>
2023-03-01 10:27:42 -0300

    refactor: req 13 - routes - role

--- app/backend/src/routes/user.routes.ts
+++ app/backend/src/routes/user.routes.ts
@@ -2,9 +2,11 @@ import { Request, Response, Router } from 'express';
 import UserService from '../services/UserService';
 import UserController from '../controllers/UserController';
 import ValidLogin from '../middlewares/ValidLogin';
+import Auth from '../middlewares/Auth';
 
 const routes = Router();
 
+const auth = new Auth();
 const validLogin = new ValidLogin();
 const userService = new UserService();
 const userController = new UserController(userService);
@@ -12,6 +14,12 @@ const userController = new UserController(userService);
 routes.post(
   '/',
   validLogin.validate,
+  (req: Request, res: Response) => userController.login(req, res),
+);
+
+routes.get(
+  '/role',
+  auth.tokenValidation,
   (req: Request, res: Response) => userController.login(req, res),
 );
 
