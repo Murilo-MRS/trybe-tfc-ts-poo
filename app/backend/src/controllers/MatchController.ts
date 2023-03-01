39b9aa61df998a35250c3d3d07e384855dac454a
Murilo-MRS <murilosantana1993@gmail.com>
2023-03-01 17:54:34 -0300

    feat: req 15 e 16 - controller - matches

---
+++ app/backend/src/controllers/MatchController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import IMatchService from '../interfaces/IMatchService';
+
+class MatchController {
+  private _service: IMatchService;
+
+  constructor(service: IMatchService) {
+    this._service = service;
+  }
+
+  public async getAllMatches(req: Request, res: Response) {
+    const query = req.query.inProgress;
+
+    const { status, message } = await this._service.findAll(query as string);
+
+    return res.status(status).json(message);
+  }
+}
+
+export default MatchController;
