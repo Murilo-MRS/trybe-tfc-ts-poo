feba7386e3319ac26a9863378d83191a83dbb404
Murilo-MRS <murilosantana1993@gmail.com>
2023-03-02 15:15:52 -0300

    feat: req 23 - routes - home,away e leaderboard

--- app/backend/src/routes/index.routes.ts
+++ app/backend/src/routes/index.routes.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import teamRouter from './team.routes';
 import userRouter from './user.routes';
 import matchRouter from './match.routes';
+import leaderboardRouter from './leaderboard.routes';
 
 const routes = Router();
 
 routes.use('/teams', teamRouter);
 routes.use('/login', userRouter);
 routes.use('/matches', matchRouter);
+routes.use('/leaderboard', leaderboardRouter);
 
 export default routes;
---
+++ app/backend/src/routes/leaderboard.routes.ts
@@ -0,0 +1,29 @@
+import { Request, Response, Router } from 'express';
+import LeaderBoardService from '../services/LeaderBoardService';
+import LeaderBoardController from '../controllers/LeaderBoardController';
+// import ValidLogin from '../middlewares/ValidLogin';
+// import Auth from '../middlewares/Auth';
+
+const routes = Router();
+
+// const auth = new Auth();
+// const validLogin = new ValidLogin();
+const leaderBoardService = new LeaderBoardService();
+const leaderBoardController = new LeaderBoardController(leaderBoardService);
+
+routes.get(
+  '/',
+  (req: Request, res: Response) => leaderBoardController.role(req, res),
+);
+
+routes.get(
+  '/home',
+  (req: Request, res: Response) => leaderBoardController.role(req, res),
+);
+
+routes.get(
+  '/away',
+  (req: Request, res: Response) => leaderBoardController.role(req, res),
+);
+
+export default routes;
