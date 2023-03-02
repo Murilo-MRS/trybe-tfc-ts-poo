f296e4654c22555b0b3e61d4e1b1f11837ae935c
Murilo-MRS <murilosantana1993@gmail.com>
2023-03-02 18:55:27 -0300

    feat: req 23 - interface - home

--- app/backend/src/interfaces/ILeaderBoard.ts
+++ app/backend/src/interfaces/ILeaderBoard.ts
@@ -7,6 +7,6 @@ export default interface ILeaderBoard {
   totalLosses: number;
   goalsFavor: number;
   goalsOwn: number;
-  goalsBalance?: number;
-  efficiency?: number;
+  goalsBalance: number;
+  efficiency: number;
 }
